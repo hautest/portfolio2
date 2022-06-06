edab364771520f0c08edad904f393ef65761ab1d
LeeSeungHun <dos07008@naver.com>
2022-06-06 19:09:39 +0900

    feat:style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from "./styles/GlobalStyle";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GlobalStyle />
+      안녕하세요
+    </>
+  );
 }
 
 export default App;
